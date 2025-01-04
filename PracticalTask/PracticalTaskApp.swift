2de84fbc6baf59e73ec9e25806a3219196d8fd8e
Sujoy <sujoy626@gmail.com>
2025-01-04 11:53:00 +0530

    Initial Commit

---
+++ PracticalTask/PracticalTaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  PracticalTaskApp.swift
+//  PracticalTask
+//
+//  Created by Sujoy Adhikary on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticalTaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
